e992798027457050f2a5f31c6b7b3a33f2e3ae23
mdsakibhr7 <mdsakibhr7@gmail.com>
2023-08-09 12:28:38 +0600

    fix errors

--- elementor-custom-widget.php
+++ elementor-custom-widget.php
@@ -16,37 +16,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+class Elementor_Customs_Widget_Plugin {
 
-public function __construct() {
-    parent::__construct();
+	public function __construct() {
+		add_action( 'elementor/frontend/after_enqueue_styles', array( $this, 'enqueue_styles' ) );
+		add_action( 'elementor/frontend/after_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 
-    // Enqueue CSS and JS
-    add_action( 'elementor/frontend/after_enqueue_styles', array( $this, 'enqueue_styles' ) );
-    add_action( 'elementor/frontend/after_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'elementor/widgets/register', array( $this, 'register_customs_widget' ) );
+	}
+
+	public function enqueue_styles() {
+		wp_enqueue_style( 'my-elementor-widget-style', plugins_url( '/assets/css/widget-style.css', __FILE__ ) );
+	}
+
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'my-elementor-widget-script', plugins_url( '/assets/js/widget-script.js', __FILE__ ), array( 'jquery' ), '', true );
+	}
+
+	/**
+	 * Register List Widget.
+	 *
+	 * Include widget file and register widget class.
+	 *
+	 * @param \Elementor\Widgets_Manager $widgets_manager Elementor widgets manager.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
+	public function register_customs_widget( $widgets_manager ) {
+		require_once( __DIR__ . '/widgets/list-widget.php' );
+
+		$widgets_manager->register( new \Elementor_Our_Team_Widget() );
+	}
 }
 
-public function enqueue_styles() {
-    wp_enqueue_style( 'my-elementor-widget-style', plugins_url( '/assets/css/widget-style.css', __FILE__ ) );
-}
-
-public function enqueue_scripts() {
-    wp_enqueue_script( 'my-elementor-widget-script', plugins_url( '/assets/js/widget-script.js', __FILE__ ), array( 'jquery' ), '', true );
-}
-
-/**
- * Register List Widget.
- *
- * Include widget file and register widget class.
- *
- * @param \Elementor\Widgets_Manager $widgets_manager Elementor widgets manager.
- *
- * @return void
- * @since 1.0.0
- */
-function register_customs_widget( $widgets_manager ) {
-	require_once( __DIR__ . '/widgets/list-widget.php' );
-
-	$widgets_manager->register( new \Elementor_Our_Team_Widget() );
-}
-
-add_action( 'elementor/widgets/register', 'register_customs_widget' );
+new Elementor_Customs_Widget_Plugin();
